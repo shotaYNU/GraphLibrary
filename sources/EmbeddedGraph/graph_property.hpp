a6b0925d27e2b7df81669b8de946a6a317a050a8
shotaYNU <ogano-shota-vs@ynu.jp>
2016-12-06 17:29:03 +0900

    Add GraphProperty.hpp

---
+++ sources/EmbeddedGraph/graph_property.hpp
@@ -0,0 +1,16 @@
+#ifndef GraphProperty_hpp
+#define GraphProperty_hpp
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+struct GraphProperty {
+    static const int MAX_VERTEX = 52;
+    static const int MAX_EDGE = 450;
+};
+
+#endif
