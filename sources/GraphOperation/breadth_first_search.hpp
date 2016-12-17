9072c5e1b10d8e05755e4c896effb23525ae9935
shotaYNU <ogano-shota-vs@ynu.jp>
2016-12-17 20:54:31 +0900

    Add breadth_first_search

---
+++ sources/GraphOperation/breadth_first_search.hpp
@@ -0,0 +1,31 @@
+#ifndef BreadthFirstSearch_hpp
+#define BreadthFirstSearch_hpp
+
+#include "../EmbeddedGraph/embedded_graph.hpp"
+
+class BreadthFirstSearch {
+public:
+    //Constructors and a destructor.
+    BreadthFirstSearch();
+    BreadthFirstSearch(EmbeddedGraph* _graph);
+    ~BreadthFirstSearch();
+
+    //Methods to BreadthFirstSearch
+    void init(EmbeddedEdge* _start, bool _clockwise);
+    bool hasNext() const { return nowCount != endCount; }
+    EmbeddedEdge* next() { return order[nowCount++]; }
+
+protected:
+
+private:
+    EmbeddedGraph* searchGraph;
+    EmbeddedEdge *start;
+    EmbeddedEdge* waiting[GraphProperty::MAX_EDGE];
+    EmbeddedEdge* order[GraphProperty::MAX_EDGE];
+    bool visited[GraphProperty::MAX_VERTEX] = { false };
+    bool clockwise;
+    int endCount, nowCount;
+
+};
+
+#endif
