d99f5d3bfdb50418e29a54011600c0b0db477594
shotaYNU <ogano-shota-vs@ynu.jp>
2017-01-10 00:56:59 +0900

    Add panel structure generation

---
+++ sources/PanelStructureGeneration/panel_structure_generation.hpp
@@ -0,0 +1,34 @@
+#ifndef PanelStrctureGenerationGeneration_hpp
+#define PanelStrctureGenerationGeneration_hpp
+
+#include "paneled_graph.hpp"
+#include "paneled_graph_representation.hpp"
+#include "embedding_set.hpp"
+
+class PanelStrctureGeneration {
+public:
+    PanelStrctureGeneration();
+    ~PanelStrctureGeneration();
+
+    void generate(PaneledGraph* _graph);
+    int getPanelStrcturesNum() { return (int)graphRepresentations.size(); }
+
+protected:
+
+private:
+    PaneledGraph* graph;
+    vector<PaneledGraphRepresentation> graphRepresentations;
+    vector<vector<bool>> embeddingMatrix;
+    EmbeddingSet* embeddingSet;
+    vector<PaneledFace*> faces;
+
+    void addIfNotExist(const vector<int>& _selectIndxies, int _embeddingNum);
+    bool isFullPanel(const vector<PaneledFace*>& _faces);
+    bool contains(const PaneledGraphRepresentation& _newGraphRepresentation);
+    vector<bool> getProductSet(const vector<bool>& _set1, const vector<bool>& _set2) const;
+    tuple<vector<int>, vector<int>, int> select(const vector<int>& _selectIndexies) const;
+    void settingRecursive(const vector<int>& _selectedIndexies, const vector<int>& _restIndexies);
+
+};
+
+#endif
