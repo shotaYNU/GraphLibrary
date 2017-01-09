3e6d7edc13cd8e674277d621bccf37227e6f315c
shotaYNU <ogano-shota-vs@ynu.jp>
2017-01-10 00:49:24 +0900

    Modify save with data method

--- sources/PaneledGraph/paneled_graph.cpp
+++ sources/PaneledGraph/paneled_graph.cpp
@@ -53,7 +53,7 @@ void PaneledGraph::saveGraph(string _filepath)
 }
 
 
-void PaneledGraph::saveGraph(string _filepath, vector<pair<string, string>> _withData)
+void PaneledGraph::saveGraph(string _filepath, vector<pair<string, picojson::value>> _withData)
 {
     ofstream outfile(_filepath);
     map<string, picojson::value> data;
@@ -65,8 +65,8 @@ void PaneledGraph::saveGraph(string _filepath, vector<pair<string, string>> _wit
     data["faces"] = picojson::value(toFacesString());
     for (auto onedata : _withData) {
         string key = get<0>(onedata);
-        string value = get<1>(onedata);
-        data[key] = picojson::value(value);
+        picojson::value value = get<1>(onedata);
+        data[key] = value;
     }
 
     picojson::value v(data);
