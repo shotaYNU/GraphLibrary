b5c8c67af9e6fe9e44b7b172d19252ceb1a6ce2d
shotaYNU <ogano-shota-vs@ynu.jp>
2017-02-12 20:52:13 +0900

    Modify to check faces other than triangle faces

--- sources/PaneledGraph/paneled_isomorphism.cpp
+++ sources/PaneledGraph/paneled_isomorphism.cpp
@@ -52,57 +52,31 @@ void PaneledIsomorphism::setMappedAdjacents()
 //    End: Set adjacency matrix
 
 //    Start: Set panel
-    vector<long long> panel;
+    vector<vector<int>> panel;
     for (int i = 0; i < graph->getVerticesNum(); ++i) {
         ed = end = graph->getVertex(i)->getFirstEdge();
         do {
             if (((PaneledFace*)ed->getNextFace())->getPanel()) {
                 vector<int> face = ed->getNextFace()->getFace();
-                long long f = 0ULL;
-                for (auto v : face)
-                    f += (1ULL << v);
-                panel.push_back(f);
+                if (find(panel.begin(), panel.end(), face) == panel.end())
+                    panel.push_back(face);
             }
             ed = ed->getNext();
         } while(ed != end);
     }
-    removeList.clear();
-    for (int i = 0; i < panel.size(); ++i) {
-        for (int j = i + 1; j < panel.size(); ++j) {
-            long long set = panel[i] & panel[j];
-            int count = 0;
-            for (int c = 0; c < ADJACENT_MAX; c++)
-                if ((set & (1ULL << c)) != 0)
-                    count++;
-            if (count >= 3) {
-                removeList.push_back(j);
-            } else if (count >= 2) {
-                removeList.push_back(i);
-                removeList.push_back(j);
-            }
-        }
-    }
-    sort(removeList.begin(), removeList.end());
-    vector<int> lists;
-    int prev = -1;
-    for (int i = 0; i < removeList.size(); ++i) {
-        if (prev != removeList[i]) {
-            lists.push_back(removeList[i]);
-            prev = removeList[i];
-        }
-    }
-    reverse(lists.begin(), lists.end());
-    for (auto index : lists)
-        panel.erase(panel.begin() + index);
 
-    vector<long long> oPanel;
+//    Set concate faces
+    panel = getConvertedFaces(panel);
+    
+//    Edit panels since there may be empty verteices.
+    vector<vector<int>> oPanel;
     for (auto p : panel) {
-        oPanel.push_back(0ULL);
-        for (int i = 0; i < ADJACENT_MAX; ++i)
-            if ((p & (1ULL << i)) != 0) {
-                int index = (int)(find(realAdjacent.begin(), realAdjacent.end(), i) - realAdjacent.begin());
-                oPanel.back() += (1ULL << index);
-            }
+        vector<int> newPanel;
+        for (auto v : p) {
+            int index = (int)(find(realAdjacent.begin(), realAdjacent.end(), v) - realAdjacent.begin());
+            newPanel.push_back(index);
+        }
+        oPanel.push_back(newPanel);
     }
     panel = oPanel;
 //    End: Set panel
@@ -147,36 +121,33 @@ void PaneledIsomorphism::setMappedAdjacents()
                     newAd[find(ad.begin(), ad.end(), j) - ad.begin()] = true;
             adj.push_back(newAd);
         }
-        vector<long long> pan;
+        vector<vector<int>> pan;
         for (auto p : panel) {
             bool fail = false;
-            for (int i = 0; i < ADJACENT_MAX; ++i) {
-                if ((p & (1ULL << i)) != 0) {
-                    if (find(ad.begin(), ad.end(), i) == ad.end()) {
-                        fail = true;
-                        break;
-                    }
+            for (auto v : p) {
+                if (find(ad.begin(), ad.end(), v) == ad.end()) {
+                    fail = true;
+                    break;
                 }
             }
             if (!fail)
                 pan.push_back(p);
         }
-        vector<long long> modifiedPan;
+        vector<vector<int>> modifiedPan;
         for (auto p : pan) {
-            modifiedPan.push_back(0ULL);
-            for (int i = 0; i < ADJACENT_MAX; ++i) {
-                if ((p & (1ULL << i)) != 0) {
-                    int index = (int)(find(ad.begin(), ad.end(), i) - ad.begin());
-                    modifiedPan.back() += (1ULL << index);
-                }
+            vector<int> newPanel;
+            for (auto v : p) {
+                int index = (int)(find(ad.begin(), ad.end(), v) - ad.begin());
+                newPanel.push_back(index);
             }
+            modifiedPan.push_back(newPanel);
         }
         addAdjacents(adj, modifiedPan);
     }
 //    End: Set disjoint mapped adjacent list
 }
 
-void PaneledIsomorphism::addAdjacents(vector<bool*> _adjacent, vector<long long> _panel)
+void PaneledIsomorphism::addAdjacents(vector<bool*> _adjacent, vector<vector<int>> _panel)
 {
     vector<pair<int, int>> degrees;
     for (int i = 0; i < _adjacent.size(); ++i) {
@@ -211,14 +182,15 @@ void PaneledIsomorphism::addAdjacents(vector<bool*> _adjacent, vector<long long>
         _adjacent[transposition.second] = temp;
     }
 
-    vector<long long> oPanel;
+//    Edit panels since we mapped ids in order of degrees.
+    vector<vector<int>> oPanel;
     for (auto p : _panel) {
-        oPanel.push_back(0ULL);
-        for (int i = 0; i < ADJACENT_MAX; ++i)
-            if ((p & (1ULL << i)) != 0) {
-                int index = (int)(find(degMap.begin(), degMap.end(), i) - degMap.begin());
-                oPanel.back() += (1ULL << index);
-            }
+        vector<int> newPanel;
+        for (auto v : p) {
+            int index = (int)(find(degMap.begin(), degMap.end(), v) - degMap.begin());
+            newPanel.push_back(index);
+        }
+        oPanel.push_back(newPanel);
     }
     _panel = oPanel;
 
@@ -242,11 +214,13 @@ void PaneledIsomorphism::addAdjacents(vector<bool*> _adjacent, vector<long long>
 
     array<int, GraphProperty::MAX_EDGE> newAdjacentRepresentation;
     array<int, GraphProperty::MAX_EDGE> bestAdjacentRepresentation;
-    vector<long long> newPanel = _panel;
-    vector<long long> bestPanel = _panel;
+    vector<vector<int>> newPanel = _panel;
+    vector<vector<int>> bestPanel = _panel;
     bestAdjacentRepresentation[0] = 10000;
     if (bestPanel.size() != 0)
-        bestPanel[0] = (1ULL << 21);
+        for (int i = 0; i < bestPanel[0].size(); ++i)
+            bestPanel[0][i] = 21;
+    sortPanels(bestPanel);
     vector<bool*> newAdjacent;
     for (int i = 0; i < _adjacent.size(); ++i) {
         bool* oneAdjacent = new bool[ADJACENT_MAX];
@@ -254,6 +228,7 @@ void PaneledIsomorphism::addAdjacents(vector<bool*> _adjacent, vector<long long>
             oneAdjacent[a] = false;
         newAdjacent.push_back(oneAdjacent);
     }
+    vector<int> BEST;
     for (int i = 0; i < pairs.size(); ++i) {
         for (int aindex = 0; aindex < _adjacent.size(); ++aindex)
             for (int a = 0; a < ADJACENT_MAX; ++a)
@@ -266,17 +241,28 @@ void PaneledIsomorphism::addAdjacents(vector<bool*> _adjacent, vector<long long>
             temp = newAdjacent[transposition.first];
             newAdjacent[transposition.first] = newAdjacent[transposition.second];
             newAdjacent[transposition.second] = temp;
-            Utility::allExchangeLong(transposition.first, transposition.second, newPanel);
         }
+        vector<vector<int>> oPanel;
+        for (auto p : _panel) {
+            vector<int> nP;
+            for (auto v : p) {
+                int index = (int)(find(pairs[i].begin(), pairs[i].end(), v) - pairs[i].begin());
+                nP.push_back(index);
+            }
+            oPanel.push_back(nP);
+        }
+        newPanel = oPanel;
         sort(newPanel.begin(), newPanel.end());
         setAdjacentRepresentation(newAdjacent, newAdjacentRepresentation);
         Results result = compareAdjacent(bestAdjacentRepresentation, repCount, newAdjacentRepresentation, repCount);
+        sortPanels(newPanel);
         if (result == Results::BETTER) {
             for (int r = 0; r < repCount; ++r)
                 bestAdjacentRepresentation[r] = newAdjacentRepresentation[r];
             bestPanel = newPanel;
         } else if (result == Results::EQUIVALENT && comparePanel(bestPanel, newPanel) == Results::BETTER) {
             bestPanel = newPanel;
+            BEST = pairs[i];
         }
     }
 
@@ -308,7 +294,157 @@ bool PaneledIsomorphism::isomorphic(const Isomorphism& _isomorphism) const
     return false;
 }
 
-bool PaneledIsomorphism::isomorphicOne(const array<int, GraphProperty::MAX_EDGE> &_adj1, const vector<long long> &_panel1, int _repCount1, const array<int, GraphProperty::MAX_EDGE> &_adj2, const vector<long long> &_panel2, int _repCount2) const
+vector<int> PaneledIsomorphism::getAdjacentIndexies(const vector<int>& _face, const vector<vector<int>>& _facesList) const
+{
+    vector<int> indexies;
+    for (int i = 0; i < _facesList.size(); ++i) {
+        if (
+            (find(_facesList[i].begin(), _facesList[i].end(), _face[0]) != _facesList[i].end() &&
+             find(_facesList[i].begin(), _facesList[i].end(), _face[1]) != _facesList[i].end()) ||
+            (find(_facesList[i].begin(), _facesList[i].end(), _face[1]) != _facesList[i].end() &&
+             find(_facesList[i].begin(), _facesList[i].end(), _face[2]) != _facesList[i].end()) ||
+            (find(_facesList[i].begin(), _facesList[i].end(), _face[0]) != _facesList[i].end() &&
+             find(_facesList[i].begin(), _facesList[i].end(), _face[2]) != _facesList[i].end())
+            ) {
+            indexies.push_back(i);
+        }
+    }
+    return indexies;
+}
+
+vector<vector<int>> PaneledIsomorphism::getConcatFace(const vector<vector<int>>& _facesList)
+{
+    vector<pair<int, int>> edges;
+    for (auto face : _facesList) {
+        for (int i = 0; i < 3; ++i) {
+            pair<int, int> newPair;
+            int index1 = i % 3;
+            int index2 = (i + 1) % 3;
+            if (face[index1] < face[index2])
+                newPair = make_pair(face[index1], face[index2]);
+            else
+                newPair = make_pair(face[index2], face[index1]);
+            auto duplicateIterator = find(edges.begin(), edges.end(), newPair);
+            if (duplicateIterator == edges.end())
+                edges.push_back(newPair);
+            else
+                edges.erase(duplicateIterator);
+        }
+    }
+
+    map<int, bool> mapping;
+    for (auto ed : edges) {
+        mapping[ed.first] = false;
+        mapping[ed.second] = false;
+    }
+    vector<vector<int>> sequencesSet;
+    while (find_if(mapping.begin(), mapping.end(), [](const pair<int, bool>& pair) { return !pair.second; }) != mapping.end()) {
+        vector<int> sequence;
+        int start = -1;
+        for (auto m : mapping)
+            if (!m.second)
+                start = m.first;
+        vector<pair<int, int>> temp2;
+        for (auto ed : edges)
+            if (ed.first == start || ed.second == start)
+                temp2.push_back(ed);
+        int end = (temp2.front().first == start) ? temp2.front().second : temp2.front().first;
+        while (sequence.size() < edges.size()) {
+            sequence.push_back(start);
+            mapping[start] = true;
+            vector<int> candidates;
+            for (auto ed : edges) {
+                if ((end == ed.first || end == ed.second) && (start != ed.first && start != ed.second)) {
+                    if (end == ed.first)
+                        candidates.push_back(ed.second);
+                    else
+                        candidates.push_back(ed.first);
+                }
+            }
+            if (candidates.size() == 1) {
+                start = end;
+                end = candidates[0];
+            } else {
+                int setId = getFromCandiate(end, start, _facesList, candidates, edges);
+                start = end;
+                end = setId;
+            }
+        }
+        int removeIndex = -1;
+        for (int i = 2; i < sequence.size() - 1; ++i)
+            if (sequence[i] == sequence[0] && sequence[i + 1] == sequence[1])
+                removeIndex = i;
+        if (removeIndex != -1)
+            sequence.erase(sequence.begin() + removeIndex, sequence.end());
+        sequencesSet.push_back(sequence);
+    }
+
+    return sequencesSet;
+}
+
+int PaneledIsomorphism::getFromCandiate(int _start, int _end, const vector<vector<int>>& _faces, const vector<int>& _candiate, const vector<pair<int, int>>& _remainEds)
+{
+    int prev = -1;
+    vector<int> adjacentFace;
+    int loopDetectCount = 0;
+    while (true) {
+        for (auto f : _faces) {
+            if (find(f.begin(), f.end(), _start) != f.end() &&
+                find(f.begin(), f.end(), _end) != f.end() &&
+                find(f.begin(), f.end(), prev) == f.end())
+                adjacentFace = f;
+        }
+        int other = -1;
+        if ((adjacentFace[0] == _start && adjacentFace[1] == _end) || (adjacentFace[0] == _end && adjacentFace[1] == _start))
+            other = adjacentFace[2];
+        else if ((adjacentFace[1] == _start && adjacentFace[2] == _end) || (adjacentFace[1] == _end && adjacentFace[2] == _start))
+            other = adjacentFace[0];
+        else if ((adjacentFace[0] == _start && adjacentFace[2] == _end) || (adjacentFace[0] == _end && adjacentFace[2] == _start))
+            other = adjacentFace[1];
+        if (find(_candiate.begin(), _candiate.end(), other) != _candiate.end() && (find(_remainEds.begin(), _remainEds.end(), make_pair(other, _start)) != _remainEds.end() || find(_remainEds.begin(), _remainEds.end(), make_pair(_start, other)) != _remainEds.end()))
+            return other;
+        prev = _end;
+        _end = other;
+        loopDetectCount++;
+        if (loopDetectCount > 1000) {
+            cerr << "Loop detected!!" << endl;
+            exit(1);
+        }
+    }
+}
+
+vector<vector<int>> PaneledIsomorphism::getConvertedFaces(vector<vector<int>> _facesList)
+{
+    queue<vector<int>> stock;
+    vector<vector<vector<int>>> facesSet;
+    while (_facesList.size() > 0) {
+        vector<int> face = _facesList[0];
+        _facesList.erase(_facesList.begin());
+        facesSet.push_back(vector<vector<int>>());
+        facesSet.back().push_back(face);
+        stock.push(face);
+        while (!stock.empty()) {
+            face = stock.front();
+            stock.pop();
+            vector<int> indexies = getAdjacentIndexies(face, _facesList);
+            for (int i = (int)indexies.size() - 1; i >= 0; --i) {
+                stock.push(_facesList[indexies[i]]);
+                facesSet.back().push_back(_facesList[indexies[i]]);
+                _facesList.erase(_facesList.begin() + indexies[i]);
+            }
+        }
+    }
+
+    vector<vector<int>> newFaces;
+    for (auto faces : facesSet) {
+        auto results = getConcatFace(faces);
+        copy(results.begin(), results.end(), back_inserter(newFaces));
+    }
+
+    return newFaces;
+}
+
+bool PaneledIsomorphism::isomorphicOne(const array<int, GraphProperty::MAX_EDGE> &_adj1, const vector<vector<int>> &_panel1, int _repCount1, const array<int, GraphProperty::MAX_EDGE> &_adj2, const vector<vector<int>> &_panel2, int _repCount2) const
 {
     if (compareAdjacent(_adj1, _repCount1, _adj2, _repCount2) == Results::EQUIVALENT)
         if (comparePanel(_panel1, _panel2) == Results::EQUIVALENT)
@@ -330,17 +466,46 @@ Isomorphism::Results PaneledIsomorphism::compareAdjacent(const array<int, GraphP
     return Results::EQUIVALENT;
 }
 
-Isomorphism::Results PaneledIsomorphism::comparePanel(const vector<long long>& _p1, const vector<long long>& _p2) const
+Isomorphism::Results PaneledIsomorphism::comparePanel(const vector<vector<int>>& _p1, const vector<vector<int>>& _p2) const
 {
     if (_p1.size() != _p2.size()) return Results::FAIL;
     for (int i = 0; i < _p1.size(); ++i) {
-        if (_p1[i] < _p2[i])
-            return Results::FAIL;
-        else if (_p1[i] > _p2[i])
-            return Results::BETTER;
+        if (_p1[i].size() < _p2[i].size()) return Results::FAIL;
+        else if (_p1[i].size() > _p2[i].size()) return Results::BETTER;
+        for (int j = 0; j < _p1[i].size(); ++j) {
+            if (_p1[i][j] < _p2[i][j])
+                return Results::FAIL;
+            else if (_p1[i][j] > _p2[i][j])
+                return Results::BETTER;
+        }
     }
 
     return Results::EQUIVALENT;
+}
+
+void PaneledIsomorphism::sortPanels(vector<vector<int>>& _panels)
+{
+    for (int i = 0; i < _panels.size(); ++i)
+        _panels[i] = sortedPanel(_panels[i]);
+    sort(_panels.begin(), _panels.end(), [](const vector<int>& a, const vector<int>& b) -> bool {
+        int firstA = a[0];
+        int secondA = a[1];
+        int firstB = b[0];
+        int secondB = b[1];
+        return firstA + secondA * 1000 < firstB + secondB * 1000;
+    });
+}
+
+vector<int> PaneledIsomorphism::sortedPanel(vector<int>& _panel)
+{
+    int minIndex = (int)(min_element(_panel.begin(), _panel.end()) - _panel.begin());
+    int direct = (_panel[(minIndex + 1) % _panel.size()] < _panel[(minIndex - 1 + _panel.size()) % _panel.size()]) ? 1 : -1;
+    vector<int> sorted;
+    sorted.push_back(_panel[minIndex]);
+    for (int i = (minIndex + direct + (int)_panel.size()) % _panel.size(); i != minIndex; i = (i + direct + (int)_panel.size()) % _panel.size())
+        sorted.push_back(_panel[i]);
+
+    return sorted;
 }
 
 vector<pair<string, string>> PaneledIsomorphism::toStringDatas()
@@ -352,8 +517,9 @@ vector<pair<string, string>> PaneledIsomorphism::toStringDatas()
             repString += to_string(bestAdjacentRepresentationList[i][o]);
         string panelString = "";
         for (auto onePanel: panelsList[i]) {
-            panelString += to_string(onePanel);
-            panelString += ",";
+            for (auto v : onePanel)
+                panelString += to_string(v) + ":";
+            panelString.back() = ',';
         }
         panelString.pop_back();
         pairs.push_back(make_pair(repString, panelString));
@@ -372,12 +538,17 @@ void PaneledIsomorphism::setPaneledIsomorphismDatas(const vector<pair<string, st
             oneRep[o] = repString[o] - '0';
 
         vector<string> panelStrings = Utility::split(panelString, ',');
-        vector<long long> onePanel;
-        for (int o = 0; o < panelStrings.size(); ++o)
-            onePanel.push_back(atoll(panelStrings[o].c_str()));
+        vector<vector<int>> onePanels;
+        for (int o = 0; o < panelStrings.size(); ++o) {
+            vector<int> ps;
+            vector<string> vertices = Utility::split(panelStrings[o], ':');
+            for (auto v : vertices)
+                ps.push_back(atoi(v.c_str()));
+            onePanels.push_back(ps);
+        }
 
         bestAdjacentRepresentationList.push_back(oneRep);
-        panelsList.push_back(onePanel);
+        panelsList.push_back(onePanels);
         repCounts.push_back((int)repString.size());
     }
 }
